68875ed206b7fe863bc8352d122c221eba737d7c
ade-ryzki <ade.ryzki@gmail.com>
2022-09-16 17:18:05 +0700

    change image

--- src/components/navbarlogin/index.jsx
+++ src/components/navbarlogin/index.jsx
@@ -25,7 +25,7 @@ const Navbarlogin = () => {
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src="https://placeimg.com/80/80/people" />
+                <Image src="https://placeImage.com/80/80/people" />
               </div>
             </label>
             <ul
